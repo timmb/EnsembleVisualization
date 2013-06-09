bfa58d77fa446fbf18345376e41c0ae92386dd69
Tim <tim@timmb.com>
2013-06-09 09:46:45 +0100

    Fixed up basic class structure

--- src/Renderer.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  Renderer.cpp
-//  EnsembleVisualization
-//
-//  Created by Tim Murray-Browne on 08/06/2013.
-//
-//
-
-#include "Renderer.h"
--- src/State.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  State.cpp
-//  EnsembleVisualization
-//
-//  Created by Tim Murray-Browne on 08/06/2013.
-//
-//
-
-#include "State.h"
